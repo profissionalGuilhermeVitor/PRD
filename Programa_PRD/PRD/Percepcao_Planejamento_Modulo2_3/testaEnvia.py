841e6538b53c72008c06e1d1ecab9eef1469c454
unknown <profissional.guilhermevitor@gmail.com>
2024-01-21 12:01:44 -0300

    Alterações no comando do Subprocess

--- Programa_PRD/PRD/Percepcao_Planejamento_Modulo2_3/testaEnvia.py
+++ Programa_PRD/PRD/Percepcao_Planejamento_Modulo2_3/testaEnvia.py
@@ -4,7 +4,7 @@ import sys
 import requests
 import graphlib 
 import json
-result =subprocess.run(["java", "-jar", r"c:\Users\Home\Desktop\teste\pddl4j\pddl4j-3.8.3.jar","-o", r"C:\Users\Home\GitHub_Files\SISTEMAS_PRD_PNRD_iPNRD\Programa_PRD\PRD\Percepcao_Planejamento_Modulo2_3\domain.pddl", "-f", r"C:\Users\Home\GitHub_Files\SISTEMAS_PRD_PNRD_iPNRD\Programa_PRD\PRD\Percepcao_Planejamento_Modulo2_3\problem.pddl"])
+result =subprocess.run(["java", "-jar", r"C:\Users\Home\GitHub_Files\SISTEMAS_PRD_PNRD_iPNRD\Programa_PRD\PRD\Percepcao_Planejamento_Modulo2_3\planejador\pddl4j\pddl4j-3.8.3.jar","-o",r"C:\Users\Home\GitHub_Files\SISTEMAS_PRD_PNRD_iPNRD\Programa_PRD\PRD\Percepcao_Planejamento_Modulo2_3\TestesExemplo\domain.pddl", "-f", r"C:\Users\Home\GitHub_Files\SISTEMAS_PRD_PNRD_iPNRD\Programa_PRD\PRD\Percepcao_Planejamento_Modulo2_3\TestesExemplo\problem.pddl","-p","0"])
 print(result)
 
 
